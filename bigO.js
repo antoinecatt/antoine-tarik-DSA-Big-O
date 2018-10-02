e20b479ca4df1aebba9ede20d41d92b1c305d061
Antoine Catt <antoinecatt@gmail.com>
2018-10-02 08:33:08 -0700

    first 4 problems solved

--- bigO.js
+++ bigO.js
@@ -6,3 +6,38 @@ function isEven(value) {
 }
 
 // answer: O(1) - Constant
+
+// Are you here?
+function areYouHere(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    const el1 = arr1[i];
+    for (let j = 0; j < arr2.length; j++) {
+      const el2 = arr2[j];
+      if (el1 === el2) return true;
+    }
+  }
+  return false;
+}
+
+// answer: O(n^2) - Polynomial
+
+// Doubler
+function doubleArrayValues(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] *= 2;
+  }
+  return array;
+}
+
+// answer: O(n) - Linear
+
+// Naive Search
+function naiveSearch(array, item) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return i;
+    }
+  }
+}
+
+// answer: O(n) - Linear
