172bb6c2820b499e621f8112a116f88a1ac0c540
Tarik Desire <tprdesire@gmail.com>
2018-10-02 14:30:24 -0400

    Finished big O for recursive drills

--- bigO.js
+++ bigO.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 // Even or odd
 function isEven(value) {
@@ -49,7 +49,7 @@ function naiveSearch(array, item) {
 function createPairs(arr) {
   for (let i = 0; i < arr.length; i++) {
     for (let j = i + 1; j < arr.length; j++) {
-      console.log(arr[i] + ", " + arr[j]);
+      console.log(arr[i] + ', ' + arr[j]);
     }
   }
 }
@@ -138,7 +138,7 @@ function isPrime(n) {
 function countSheep(num) {
   //stopping condition of base case
   if (num === 0) {
-    console.log(`All sheep jumped over the fence`);
+    console.log('All sheep jumped over the fence');
   }
   //this is the recursive case
   //this will be executed until it reaches base case
@@ -202,8 +202,167 @@ function convertToBinary(num) {
     //carry the reminder to the next recursion call
     return convertToBinary(Math.floor(num / 2)) + binary;
   } else {
-    return ""; //base case - at some point the divisions will lead to 0
+    return ''; //base case - at some point the divisions will lead to 0
   }
 }
 
 // O(log(n)) - logarithmic
+
+
+//Exercise 7 - Factorial
+function factorial(n) {  
+  // Base Case - when n is equal to 0, we stop the recursion
+  if (n === 0) {
+    return 1;
+  }
+  // This is our Recursive Case
+  // It will run for all other conditions except when n is equal to 0
+  return n * factorial(n - 1);
+}
+
+//answer: O(n) - linear
+
+
+//Exercise 8 - Fibonacci
+function fibonacci(n) {
+  // Base case
+  if (n <= 0) {
+    return 0;
+  }
+  // Base case
+  if (n <= 2) {
+    return 1;
+  }	
+  // Recursive case
+  return fibonacci(n - 1) + fibonacci(n - 2);	
+}
+//answer: O(2^n) - Exponential
+
+
+//Exercise 9 - Anagrams
+function anagrams(prefix, str){
+  if(str.length <= 1){
+    console.log(`The anagram is ${prefix}${str}`);
+  } else {
+    for(let i=0; i<str.length; i++){
+      let currentLetter = str.substring(i, i+1); 
+      let previousLetters = str.substring(0,i);
+      let afterLetters = str.substring(i+1);
+      anagrams(prefix+currentLetter, previousLetters+afterLetters);
+    }
+  }
+}
+function printAnagram(word){
+  //console.log(`The word for which we will find an anagram is ${word}`);
+  anagrams(' ', word);
+
+}
+
+//answer: O(n) - Linear
+
+
+//Exercise 10 - Animal Hierarchy
+const animalHierarchy = [
+  {id: 'Animals', parent: null},
+  {id: 'Mammals', parent: 'Animals'},
+  {id: 'Dogs', parent:'Mammals' },
+  {id: 'Cats', parent:'Mammals' },
+  {id: 'Golden Retriever', parent: 'Dogs'},
+  {id: 'Husky', parent:'Dogs' },
+  {id: 'Bengal', parent:'Cats' }
+];
+
+// ==============================
+function traverse(animalHierarchy, parent) {
+  let node = {};
+  animalHierarchy
+    .filter(item => item.parent === parent)
+    .forEach(item => node[item.id] = traverse(animalHierarchy, item.id));
+  return node;  
+}
+
+//answer: O(n) - Linear
+
+//Exercise 11 - Organization Chart
+let organization = {
+  'Zuckerberg': {		
+    'Schroepfer': {
+      'Bosworth': {
+        'Steve':{},
+        'Kyle':{},
+        'Andra':{}
+      },
+      'Zhao': {
+        'Richie':{},
+        'Sofia':{},
+        'Jen':{}
+      },
+      'Badros': {
+        'John':{},
+        'Mike':{},
+        'Pat':{}
+      },
+      'Parikh': {
+        'Zach':{},
+        'Ryan':{},
+        'Tes':{}
+      }
+    },
+    'Schrage': {
+      'VanDyck': {
+        'Sabrina':{},
+        'Michelle':{},
+        'Josh':{}
+      },
+      'Swain': {
+        'Blanch':{},
+        'Tom':{},
+        'Joe':{}
+      },
+      'Frankovsky': {
+        'Jasee':{},
+        'Brian':{},
+        'Margaret':{}
+      }
+    },
+    'Sandberg': {
+      'Goler': {
+        'Eddie':{},
+        'Julie':{},
+        'Annie':{}
+      },
+      'Hernandez': {
+        'Rowi':{},
+        'Inga':{},
+        'Morgan':{}
+      },
+      'Moissinac': {
+        'Amy':{},
+        'Chuck':{},
+        'Vinni':{}
+      },
+      'Kelley': {
+        'Eric':{},
+        'Ana':{},
+        'Wes':{}
+      }
+    }}};
+
+function traverseA(data, depth = 0) {
+  let indent = ' '.repeat(depth * 4);
+  Object.keys(data).forEach(key => {
+    console.log(indent + key);
+    traverseA(data[key], depth + 1);
+  });
+}
+
+function traverseB(node, indent=0) {
+  for (var key in node) {
+    console.log(' '.repeat(indent), key);
+    traverseB(node[key], indent + 4);
+  }
+}
+
+//answer: O(n) - Linear
+
+
